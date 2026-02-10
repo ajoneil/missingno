6faa848b6b46540bb216d25ccd695b30a9903cd5
Andrew O'Neil <andy@andyofniall.net>
2026-02-10 19:40:44 +1100

    Replace Box<dyn MemoryBankController> with Mbc enum

--- src/game_boy/cartridge/mbc/mod.rs
+++ src/game_boy/cartridge/mbc/mod.rs
@@ -14,3 +14,73 @@ pub trait MemoryBankController {
     fn read(&self, address: u16) -> u8;
     fn write(&mut self, address: u16, value: u8);
 }
+
+pub enum Mbc {
+    NoMbc(no_mbc::NoMbc),
+    Mbc1(mbc1::Mbc1),
+    Mbc2(mbc2::Mbc2),
+    Mbc3(mbc3::Mbc3),
+    Mbc5(mbc5::Mbc5),
+    Mbc6(mbc6::Mbc6),
+    Mbc7(mbc7::Mbc7),
+    Huc1(huc1::Huc1),
+    Huc3(huc3::Huc3),
+}
+
+impl MemoryBankController for Mbc {
+    fn rom(&self) -> &[u8] {
+        match self {
+            Mbc::NoMbc(m) => m.rom(),
+            Mbc::Mbc1(m) => m.rom(),
+            Mbc::Mbc2(m) => m.rom(),
+            Mbc::Mbc3(m) => m.rom(),
+            Mbc::Mbc5(m) => m.rom(),
+            Mbc::Mbc6(m) => m.rom(),
+            Mbc::Mbc7(m) => m.rom(),
+            Mbc::Huc1(m) => m.rom(),
+            Mbc::Huc3(m) => m.rom(),
+        }
+    }
+
+    fn ram(&self) -> Option<Vec<u8>> {
+        match self {
+            Mbc::NoMbc(m) => m.ram(),
+            Mbc::Mbc1(m) => m.ram(),
+            Mbc::Mbc2(m) => m.ram(),
+            Mbc::Mbc3(m) => m.ram(),
+            Mbc::Mbc5(m) => m.ram(),
+            Mbc::Mbc6(m) => m.ram(),
+            Mbc::Mbc7(m) => m.ram(),
+            Mbc::Huc1(m) => m.ram(),
+            Mbc::Huc3(m) => m.ram(),
+        }
+    }
+
+    fn read(&self, address: u16) -> u8 {
+        match self {
+            Mbc::NoMbc(m) => m.read(address),
+            Mbc::Mbc1(m) => m.read(address),
+            Mbc::Mbc2(m) => m.read(address),
+            Mbc::Mbc3(m) => m.read(address),
+            Mbc::Mbc5(m) => m.read(address),
+            Mbc::Mbc6(m) => m.read(address),
+            Mbc::Mbc7(m) => m.read(address),
+            Mbc::Huc1(m) => m.read(address),
+            Mbc::Huc3(m) => m.read(address),
+        }
+    }
+
+    fn write(&mut self, address: u16, value: u8) {
+        match self {
+            Mbc::NoMbc(m) => m.write(address, value),
+            Mbc::Mbc1(m) => m.write(address, value),
+            Mbc::Mbc2(m) => m.write(address, value),
+            Mbc::Mbc3(m) => m.write(address, value),
+            Mbc::Mbc5(m) => m.write(address, value),
+            Mbc::Mbc6(m) => m.write(address, value),
+            Mbc::Mbc7(m) => m.write(address, value),
+            Mbc::Huc1(m) => m.write(address, value),
+            Mbc::Huc3(m) => m.write(address, value),
+        }
+    }
+}
--- src/game_boy/cartridge/mod.rs
+++ src/game_boy/cartridge/mod.rs
@@ -1,15 +1,15 @@
 mod mbc;
 
 use mbc::{
-    MemoryBankController, huc1::Huc1, huc3::Huc3, mbc1::Mbc1, mbc2::Mbc2, mbc3::Mbc3, mbc5::Mbc5,
-    mbc6::Mbc6, mbc7::Mbc7, no_mbc::NoMbc,
+    Mbc, MemoryBankController, huc1::Huc1, huc3::Huc3, mbc1::Mbc1, mbc2::Mbc2, mbc3::Mbc3,
+    mbc5::Mbc5, mbc6::Mbc6, mbc7::Mbc7, no_mbc::NoMbc,
 };
 
 pub struct Cartridge {
     title: String,
     has_battery: bool,
     sgb_flag: bool,
-    mbc: Box<dyn MemoryBankController>,
+    mbc: Mbc,
 }
 
 impl Cartridge {
@@ -31,17 +31,17 @@ impl Cartridge {
         );
         let save = if has_battery { save_data } else { None };
 
-        let mbc: Box<dyn MemoryBankController> = match cartridge_type {
-            0x00 | 0x08 | 0x09 => Box::new(NoMbc::new(rom, save)),
-            0x01..=0x03 => Box::new(Mbc1::new(rom, save)),
-            0x05 | 0x06 => Box::new(Mbc2::new(rom, save)),
-            0x0f..=0x13 => Box::new(Mbc3::new(rom, save)),
-            0x19..=0x1b => Box::new(Mbc5::new(rom, save)),
-            0x1c..=0x1e => Box::new(Mbc5::new_rumble(rom, save)),
-            0x20 => Box::new(Mbc6::new(rom, save)),
-            0x22 => Box::new(Mbc7::new(rom, save)),
-            0xfe => Box::new(Huc3::new(rom, save)),
-            0xff => Box::new(Huc1::new(rom, save)),
+        let mbc = match cartridge_type {
+            0x00 | 0x08 | 0x09 => Mbc::NoMbc(NoMbc::new(rom, save)),
+            0x01..=0x03 => Mbc::Mbc1(Mbc1::new(rom, save)),
+            0x05 | 0x06 => Mbc::Mbc2(Mbc2::new(rom, save)),
+            0x0f..=0x13 => Mbc::Mbc3(Mbc3::new(rom, save)),
+            0x19..=0x1b => Mbc::Mbc5(Mbc5::new(rom, save)),
+            0x1c..=0x1e => Mbc::Mbc5(Mbc5::new_rumble(rom, save)),
+            0x20 => Mbc::Mbc6(Mbc6::new(rom, save)),
+            0x22 => Mbc::Mbc7(Mbc7::new(rom, save)),
+            0xfe => Mbc::Huc3(Huc3::new(rom, save)),
+            0xff => Mbc::Huc1(Huc1::new(rom, save)),
 
             _ => panic!("nyi: mbc {:2x}", cartridge_type),
         };
