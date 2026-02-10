7850b6e941289b2f5d8ef796bb08d4f3e67f8b98
Andrew O'Neil <andy@andyofniall.net>
2026-02-11 09:00:04 +1100

    Fix EI: don't reset IME delay when already enabled
    EI followed by EI was ping-ponging between EnableAfterNextInstruction
    and Enabled, preventing interrupts from ever firing. Now EI is a no-op
    when IME is already Enabled.
    
    Fixes mooneye ei_sequence. Test results: 39/86 -> 40/86.

--- src/game_boy/cpu/execute/interrupt.rs
+++ src/game_boy/cpu/execute/interrupt.rs
@@ -8,7 +8,10 @@ impl Cpu {
     pub fn execute_interrupt(&mut self, instruction: Interrupt) -> OpResult {
         match instruction {
             Interrupt::Enable => {
-                self.interrupt_master_enable = InterruptMasterEnable::EnableAfterNextInstruction;
+                if self.interrupt_master_enable != InterruptMasterEnable::Enabled {
+                    self.interrupt_master_enable =
+                        InterruptMasterEnable::EnableAfterNextInstruction;
+                }
                 OpResult::cycles(1)
             }
             Interrupt::Disable => {
